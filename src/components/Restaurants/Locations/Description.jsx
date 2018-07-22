b2949722e4e8810444abb8de5f3651fb2f5f8864
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-23 02:39:20 +0300

    Description added

--- src/components/Restaurants/Locations/Description.jsx
+++ src/components/Restaurants/Locations/Description.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Description = (/* ocation */) => (
-  <div>
-    blaa
-    {/* <h2>{this.location.name}</h2>
-    <p>{this.location.desc}</p> */}
-  </div>
-);
+const Description = ({ location }) =>
+  location.Name ? (
+    <div>
+      <h2>{location.Name}</h2>
+      <p>{location.Desc}</p>
+      <img src={location.Photo} alt={location.Name} />
+    </div>
+  ) : (
+    <div>Please select a location.</div>
+  );
+
+Description.propTypes = {
+  location: PropTypes.shape({
+    Name: PropTypes.string,
+    Desc: PropTypes.string,
+    Photo: PropTypes.string
+  })
+};
+
+Description.defaultProps = {
+  location: {}
+};
 
 export default Description;
