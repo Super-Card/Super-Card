30ae965d973e433b60bc9b3d1641d4d4c8c13a61
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-23 02:38:33 +0300

    Item added

--- src/components/Restaurants/Locations/Item.jsx
+++ src/components/Restaurants/Locations/Item.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Item = ({ location }) => (
   <div>
     <button>{location.name}</button>
   </div>
 );
+
+Item.propTypes = {
+  location: PropTypes.shape({
+    Name: PropTypes.string,
+    Desc: PropTypes.string,
+    Photo: PropTypes.string
+  })
+};
+
+Item.defaultProps = {
+  location: {}
+};
 
 export default Item;
