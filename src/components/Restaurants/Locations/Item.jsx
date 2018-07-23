ffc20e8ebe5f96874fc079f082fe7d1874cd7e46
Denitsa Dobreva <ddobretc@training.local>
2018-07-23 20:06:18 +0300

    locations components progress

--- src/components/Restaurants/Locations/Item.jsx
+++ src/components/Restaurants/Locations/Item.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { observer, inject } from 'mobx-react';
+import { appStatePropType, appStatePropTypeDefaults } from '../../../stores';
 
-const Item = ({ location }) => (
-  <div>
-    <button>{location.name}</button>
-  </div>
-);
+@inject('appState')
+@observer
+class Item extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleItemSelect = this.handleItemSelect.bind(this);
+  }
+
+  handleItemSelect = location => {
+    this.props.appState.setSelectedLocation(location);
+  };
+
+  render() {
+    return <button onClick={() => this.handleItemSelect(this.props.location)}>{this.props.location.name}</button>;
+  }
+}
 
 Item.propTypes = {
   location: PropTypes.shape({
-    Name: PropTypes.string,
-    Desc: PropTypes.string,
-    Photo: PropTypes.string
-  })
+    id: PropTypes.number,
+    name: PropTypes.string,
+    desc: PropTypes.string
+  }).isRequired
 };
 
-Item.defaultProps = {
-  location: {}
+Item.wrappedComponent.propTypes = {
+  appState: appStatePropType
+};
+
+Item.wrappedComponent.defaultProps = {
+  appState: appStatePropTypeDefaults
 };
 
 export default Item;
--- src/components/Restaurants/Locations/ListItems.jsx
+++ src/components/Restaurants/Locations/ListItems.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
-import { appStatePropType, appStatePropTypeDefaults } from '../stores';
+import { appStatePropType, appStatePropTypeDefaults } from '../../../stores';
 import Item from './Item';
 
 @inject('appState')
 @observer
 class ListItems extends React.Component {
   render() {
-    return this.props.appState.getLocations ? (
-      <ul>
-        {this.props.appState.getLocations.map(location => <Item key={location.id} location={location} />)}
-      </ul>
-    ) : (
-      <ul>
-        <li>No found items.</li>
-      </ul>
+    return (
+      <div>{this.props.appState.getLocations.map(location => <Item key={location.id} location={location} />)}</div>
     );
   }
 }
