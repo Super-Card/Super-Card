803b8e1df5b3137f16ba18fef4c4336e54a11003
Plamen Draganov <pdragatc@training.local>
2018-07-23 21:17:03 +0300

    Adding some new components

---
+++ src/components/Clothes/FilteredShops/Shops.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Shops.css';
+
+const Shops = () => (
+  <div className="shop-box">
+    <div className="row">
+      <div className="item">
+        <img alt="shop" src="https://www.adore.bg/c/182-category_default/giorgio-armani.jpg" />
+        <h4>Title</h4>
+        <h4>Discaunt </h4>
+      </div>
+      <div className="item">
+        <img alt="shop" src="https://www.adore.bg/c/182-category_default/giorgio-armani.jpg" />
+        <h4>Title</h4>
+        <h4>Discaunt </h4>
+      </div>
+      <div className="item">
+        <img alt="shop" src="https://www.adore.bg/c/182-category_default/giorgio-armani.jpg" />
+        <h4>Title</h4>
+        <h4>Discaunt </h4>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="item">
+        <img alt="shop" src="https://www.adore.bg/c/182-category_default/giorgio-armani.jpg" />
+        <h4>Title</h4>
+        <h4>Discaunt</h4>
+      </div>
+      <div className="item">
+        <img alt="shop" src="https://www.adore.bg/c/182-category_default/giorgio-armani.jpg" />
+        <h4>Title</h4>
+        <h4>Discaunt </h4>
+      </div>
+      <div className="item">
+        <img alt="shop" src="https://www.adore.bg/c/182-category_default/giorgio-armani.jpg" />
+        <h4>Title</h4>
+        <h4>Discaunt </h4>
+      </div>
+    </div>
+  </div>
+);
+
+export default Shops;
--- src/components/Clothes/ShopFilter/Discaunt/Discaunt.jsx
+++ src/components/Clothes/ShopFilter/Discaunt/Discaunt.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import './Discaunt.css';
 
 const Discaunt = () => (
-    <div className="discaunt-box">
+  <div className="discaunt-box">
     <h2>Discaunt:</h2>
-	<div className="input-holder"><input type="checkbox"/>5%</div>
-	<div className="input-holder"><input type="checkbox"/>10%</div>
-	<div className="input-holder"><input type="checkbox"/>15%</div>
-	<div className="input-holder"><input type="checkbox"/>20%</div>
-	<div className="input-holder"><input type="checkbox"/>25%</div>
+    <div className="input-holder">
+      <input type="checkbox" />5%
+    </div>
+    <div className="input-holder">
+      <input type="checkbox" />10%
+    </div>
+    <div className="input-holder">
+      <input type="checkbox" />15%
+    </div>
+    <div className="input-holder">
+      <input type="checkbox" />20%
+    </div>
+    <div className="input-holder">
+      <input type="checkbox" />25%
+    </div>
   </div>
 );
 
