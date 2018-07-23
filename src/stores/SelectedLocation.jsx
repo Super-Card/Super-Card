f2cdcc90ee07a7181821623a20554c7508f8ec62
Dimitrina Beleva <40005046+dimitrina-beleva@users.noreply.github.com>
2018-07-23 19:26:41 +0300

    Added img (#33)

--- src/stores/SelectedLocation.jsx
+++ src/stores/SelectedLocation.jsx
@@ -2,61 +2,60 @@ import { observable, action, computed } from 'mobx';
 import PropTypes from 'prop-types';
 
 class AppState {
-    @computed
-    get getLocations() {
-        return this.locations;
-    }
-    
-    @observable
-    locations = [
-        { id: 1, name: 'Raffy Terrace Bar', desc: 'bla bla' },
-        { id: 2, name: 'Spaghetti Kitchen', desc: 'kla kla' },
-        { id: 3, name: 'La Bocca', desc: 'kla kla' },
-        { id: 4, name: 'BMS Cuisine', desc: 'kla kla' },
-        { id: 5, name: 'Nordsee', desc: 'kla kla' },
-        { id: 6, name: 'Casavino', desc: 'kla kla' }
-    ];
+  @computed
+  get getLocations() {
+    return this.locations;
+  }
 
-    @observable
-    selectedLocation = {};
+  @observable
+  locations = [
+    { id: 1, name: 'Raffy Terrace Bar', desc: 'bla bla' },
+    { id: 2, name: 'Spaghetti Kitchen', desc: 'kla kla' },
+    { id: 3, name: 'La Bocca', desc: 'kla kla' },
+    { id: 4, name: 'BMS Cuisine', desc: 'kla kla' },
+    { id: 5, name: 'Nordsee', desc: 'kla kla' },
+    { id: 6, name: 'Casavino', desc: 'kla kla' }
+  ];
 
-    @action
-    setSelectedLocation(location) {
-        this.selectedLocation = location;
-    } 
+  @observable selectedLocation = {};
+
+  @action
+  setSelectedLocation(location) {
+    this.selectedLocation = location;
+  }
 }
 
 const appStatePropType = PropTypes.shape({
-    locations: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number,
-        name: PropTypes.string,
-        desc: PropTypes.string,
-        photo: PropTypes.string
-      })
-    ),
-    selectedLocation: PropTypes.shape({
-        id: PropTypes.number,
-        name: PropTypes.string,
-        desc: PropTypes.string,
-        photo: PropTypes.string
-    }),
-    setSelectedLocation: PropTypes.func,
-    getLocations: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number,
-            name: PropTypes.string,
-            desc: PropTypes.string,
-            photo: PropTypes.string
-        })
-    )
+  locations: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      desc: PropTypes.string,
+      photo: PropTypes.string
+    })
+  ),
+  selectedLocation: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    desc: PropTypes.string,
+    photo: PropTypes.string
+  }),
+  setSelectedLocation: PropTypes.func,
+  getLocations: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      desc: PropTypes.string,
+      photo: PropTypes.string
+    })
+  )
 });
 
 const appStatePropTypeDefaults = {
-    locations: [],
-    selectedLocation: {},
-    getLocations: [],
-    setSelectedLocation: () => true,
+  locations: [],
+  selectedLocation: {},
+  getLocations: [],
+  setSelectedLocation: () => true
 };
 
 export { AppState, appStatePropType, appStatePropTypeDefaults };
