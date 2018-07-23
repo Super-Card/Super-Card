5d6fb5e8b908cd37196cea56960f87b8649d21e2
Dimitrina Beleva <40005046+dimitrina-beleva@users.noreply.github.com>
2018-07-23 23:10:30 +0300

    Added observer (#44)

--- src/components/Beauty/FiltersResult/ResultList.jsx
+++ src/components/Beauty/FiltersResult/ResultList.jsx
@@ -1,11 +1,26 @@
+import { observer, inject } from 'mobx-react';
 import React from 'react';
 import ResultItem from './ResultItem';
+import { beautyPropType, beautyPropTypeDefaults } from '../../../stores/Beauty';
 
-const ResultList = () => (
-  <div>
-    <p>ResultList</p>
-    <ResultItem />
-  </div>
-);
+@inject('beauty')
+@observer
+class ResultList extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.beauty.getResultItems.map(resultItem => <ResultItem key={resultItem.id} resultItem={resultItem} />)}
+      </div>
+    );
+  }
+}
+
+ResultList.wrappedComponent.propTypes = {
+  beauty: beautyPropType
+};
+
+ResultList.wrappedComponent.defaultProps = {
+  beauty: beautyPropTypeDefaults
+};
 
 export default ResultList;
