9dbd87a92d2f630eaf7114fd73e70ea3f4cd13be
Plamen Draganov <39806681+plamendraganov@users.noreply.github.com>
2018-07-23 18:57:54 +0300

    Feature/clothes components (#31)
    * Adding Clothes page
    
    * Clothes Page

---
+++ src/components/Clothes/ShopFilter/Brands.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Brands = () => (
+  <div>
+    <h2>Brands:</h2>
+    <input type="checkbox"/>Armani
+    <input type="checkbox"/>Versace
+    <input type="checkbox"/>Dolce & Gabbana
+    <input type="checkbox"/>Hugo Boss
+    <input type="checkbox"/>Prada
+  </div>
+);
+
+export default Brands;
