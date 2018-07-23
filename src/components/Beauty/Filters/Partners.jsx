811850ba39c56876ee913a620adc054789fad338
Dimitrina Beleva <dbeletc@training.local>
2018-07-23 20:02:08 +0300

    Added elements

--- src/components/Beauty/Filters/Partners.jsx
+++ src/components/Beauty/Filters/Partners.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const Partners = () => (
   <div>
     <p>Partners:</p>
-    <p>name: Partner1</p>
+    <ul>
+      <li>
+        <input type="checkbox" />Beauty Zone
+      </li>
+      <li>
+        <input type="checkbox" />Lilly Drogerie
+      </li>
+      <li>
+        <input type="checkbox" />DM
+      </li>
+      <li>
+        <input type="checkbox" />Casyopea
+      </li>
+    </ul>
   </div>
 );
 
