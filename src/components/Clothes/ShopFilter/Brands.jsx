7aef572eba7b333e30481992ebbba4c9b5c28eb4
Dimitrina Beleva <dbeletc@training.local>
2018-07-23 19:23:50 +0300

    Added img

--- src/components/Clothes/ShopFilter/Brands.jsx
+++ src/components/Clothes/ShopFilter/Brands.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Brands = () => (
   <div>
     <h2>Brands:</h2>
-    <input type="checkbox"/>Armani
-    <input type="checkbox"/>Versace
-    <input type="checkbox"/>Dolce & Gabbana
-    <input type="checkbox"/>Hugo Boss
-    <input type="checkbox"/>Prada
+    <input type="checkbox" />Armani
+    <input type="checkbox" />Versace
+    <input type="checkbox" />Dolce & Gabbana
+    <input type="checkbox" />Hugo Boss
+    <input type="checkbox" />Prada
   </div>
 );
 
