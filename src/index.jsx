7066973c71327362f87fd25846d0fdc552fec4e9
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-23 20:10:58 +0300

    locations components progress (#37)
    * locations components progress
    
    * Update .babelrc

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+import { AppState } from './stores/SelectedLocation';
 import App from './App';
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+const appState = new AppState();
+
+window.appState = appState;
+
+ReactDOM.render(
+  <Provider appState={appState}>
+    <App />
+  </Provider>,
+  document.querySelector('.container')
+);
+
+/* 
+ReactDOM.render(<App />, document.querySelector('.container')); */
