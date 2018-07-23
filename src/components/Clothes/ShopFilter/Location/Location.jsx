d30c6123b6b7b688141370b2ef03de2d4b4791ae
Plamen Draganov <pdragatc@training.local>
2018-07-23 19:52:13 +0300

    new components for Clothes

---
+++ src/components/Clothes/ShopFilter/Location/Location.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Location.css';
+
+const Location = () => (
+  <div className="location-box">
+    <h2>Location:</h2>
+	<div className="input-holder"><input type="checkbox"/>Sofia</div>
+	<div className="input-holder"><input type="checkbox"/>Plovdiv</div>
+	<div className="input-holder"><input type="checkbox"/>Varna</div>
+	<div className="input-holder"><input type="checkbox"/>Burgas</div>
+	<div className="input-holder"><input type="checkbox"/>Stara Zagora</div>
+  </div>
+);
+
+export default Location;
