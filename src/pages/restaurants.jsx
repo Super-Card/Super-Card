f4cc2cc801e29c1c9ca79dd1bcecf3d7c3d3bf0c
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-24 08:24:43 +0300

    Feature/locations styling (#47)
    * restaurants buttons styled + body margin fix
    
    * styling description
    
    * lint

--- src/pages/restaurants.jsx
+++ src/pages/restaurants.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Introduction from '../components/Restaurants/Introduction';
 import Locations from '../components/Restaurants/Locations/Locations';
 import ChatBot from '../components/Restaurants/ChatBot/ChatBot';
+import '../components/Restaurants/Locations/locations.css';
 
 const Restaurants = () => (
   <div>
