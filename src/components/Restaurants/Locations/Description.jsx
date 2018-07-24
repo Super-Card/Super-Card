359eb33da80ba1d4c8a5978d5b08aa709e8db934
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-24 17:45:14 +0300

    styled restaurant images (#49)
    * styled restaurant images
    
    * styled unselected

--- src/components/Restaurants/Locations/Description.jsx
+++ src/components/Restaurants/Locations/Description.jsx
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 const Description = ({ location }) =>
   location.name ? (
     <div className="locations-details">
-      <img src={location.photo} alt={location.name} className="locations-details__img" />
-      <h2 className="locations-details__name">{location.name}</h2>
-      <p className="locations-details__desc">{location.desc}</p>
+      <div className="locations-details__text">
+        <h2 className="locations-details__name">{location.name}</h2>
+        <p className="locations-details__desc">{location.desc}</p>
+      </div>
+      <div className="locations-details__img" style={{backgroundImage: `url(${location.photo})`}}></div>
     </div>
   ) : (
     <div className="locations-details--unselected">Please select a location.</div>
