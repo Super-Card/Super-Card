9fbdc3894f20adcf2254aca9ee2455164bac2337
Dimitrina <d_d_b@mail.bg>
2018-07-26 02:41:08 +0300

    Added resouces and rendered result item

--- src/components/Beauty/Filters/Discounts.jsx
+++ src/components/Beauty/Filters/Discounts.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Discounts = () => (
   <div>
-    <p>Discounts(%):</p>
-    <input type="range" min="5" max="50" />
+    <p>Discounts:</p>
+    <ul>
+      <li>
+        <input type="checkbox" />10%
+      </li>
+      <li>
+        <input type="checkbox" />15%
+      </li>
+      <li>
+        <input type="checkbox" />20%
+      </li>
+      <li>
+        <input type="checkbox" />25%
+      </li>
+      <li>
+        <input type="checkbox" />30%
+      </li>
+    </ul>
   </div>
 );
 
