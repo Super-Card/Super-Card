861ea297209a01d1ef7d34972380646cb7c94dc3
Martin Dobrudzhanski <martin.dobr@abv.bg>
2018-07-26 18:02:15 +0300

    some changes (#57)

--- src/components/Tech/Categories/cameras.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Camera = () => <div>Cameras</div>;
-
-export default Camera;
--- src/components/Tech/Categories/photo.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Photo = () => <div>Photo</div>;
-
-export default Photo;
