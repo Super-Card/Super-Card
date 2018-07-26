e8429b8b84022e2dc0f72d8f8e9f88a3d7855a26
Dimitrina <d_d_b@mail.bg>
2018-07-27 02:59:10 +0300

    Initial styling - beauty page

--- src/components/Beauty/Filters/Discounts.jsx
+++ src/components/Beauty/Filters/Discounts.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
+import './BeautyFilters.css';
 
 const Discounts = () => (
-  <div>
-    <p>Discounts:</p>
-    <ul>
-      <li>
-        <input type="checkbox" />10%
-      </li>
-      <li>
-        <input type="checkbox" />15%
-      </li>
-      <li>
-        <input type="checkbox" />20%
-      </li>
-      <li>
-        <input type="checkbox" />25%
-      </li>
-      <li>
-        <input type="checkbox" />30%
-      </li>
-    </ul>
+  <div className="discount-filter-beauty">
+    <h3>Discount</h3>
+    <label>
+      <input type="checkbox" name="10%" id="10%" value="10%" />10%
+    </label>
+    <label>
+      <input type="checkbox" name="15%" id="15%" value="15%" />15%
+    </label>
+    <label>
+      <input type="checkbox" name="20%" id="20%" value="20%" />20%
+    </label>
+    <label>
+      <input type="checkbox" name="25%" id="25%" value="25%" />25%
+    </label>
+    <label>
+      <input type="checkbox" name="30%" id="30%" value="30%" />30%
+    </label>
   </div>
 );
 
