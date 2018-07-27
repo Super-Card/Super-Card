3a207d33ec217db1a86de6b29c41412a6be3c532
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-27 18:22:15 +0300

    onClick confirm reservation triggers toggleModal

--- src/components/Restaurants/Reservations/ConfirmationModal.jsx
+++ src/components/Restaurants/Reservations/ConfirmationModal.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 /* import PropTypes from 'prop-types'; */
-import { observer, inject } from 'mobx-react';/* 
+import {
+  observer,
+  inject
+} from 'mobx-react'; /* 
 import { restaurantsPropType, restaurantsPropTypeDefaults } from '../../../stores/Restaurants'; */
 
 @inject('restaurants')
 @observer
 class ConfirmationModal extends React.Component {
-
+  render() {
+    return <div>heeeeey</div>;
+  }
 }
 
 export default ConfirmationModal;
