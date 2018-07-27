fb0eed1730c558c63851e55bca5d1560d1acadec
Martin Dobrudzhanski <martin.dobr@abv.bg>
2018-07-27 17:08:28 +0300

    added-provider (#65)

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
 import { Restaurants } from './stores/Restaurants';
 import { Beauty } from './stores/Beauty';
+import { Tech } from './stores/Tech';
 import App from './App';
 
 const restaurants = new Restaurants();
 const beauty = new Beauty();
+const tech = new Tech();
 
 window.restaurants = restaurants;
 window.beauty = beauty;
+window.tech = tech;
 
 ReactDOM.render(
-  <Provider restaurants={restaurants} beauty={beauty}>
+  <Provider restaurants={restaurants} beauty={beauty} tech={tech}>
     <App />
   </Provider>,
   document.querySelector('.container')
