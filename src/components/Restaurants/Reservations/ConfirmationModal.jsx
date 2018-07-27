38db7d7de9941ebf99e54acfb1b117bbb70dc2cb
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-27 11:53:33 +0300

    added ConfirmationModal component

---
+++ src/components/Restaurants/Reservations/ConfirmationModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+/* import PropTypes from 'prop-types'; */
+import { observer, inject } from 'mobx-react';/* 
+import { restaurantsPropType, restaurantsPropTypeDefaults } from '../../../stores/Restaurants'; */
+
+@inject('restaurants')
+@observer
+class ConfirmationModal extends React.Component {
+
+}
+
+export default ConfirmationModal;
