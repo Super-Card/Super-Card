4298f85982dc63fed4393683e12969cf3a755b29
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-27 19:31:39 +0300

    Feature/reservations modal (#66)
    * onClick confirm reservation triggers toggleModal
    
    * confirmation modal rendered + close button
    
    * confirmation modal styling
    
    * lint

--- src/components/Restaurants/Reservations/CompleteReservation.jsx
+++ src/components/Restaurants/Reservations/CompleteReservation.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 /* import PropTypes from 'prop-types'; */
 import { observer, inject } from 'mobx-react';
 import { restaurantsPropType, restaurantsPropTypeDefaults } from '../../../stores/Restaurants';
+import ConfirmationModal from './ConfirmationModal';
 
 @inject('restaurants')
 @observer
 class CompleteReservation extends React.Component {
   render() {
     return (
-      <div className="reservation-complete">
-        <button
-          className={this.props.restaurants.getReservedRestaurant.name ? 'reservation-confirm' : 'reservation-disabled'}
-          onClick={this.props.restaurants.getReservedRestaurant.name ? () => '' : () => ''}
-        >
-          Complete Reservation
-        </button>
-        <button onClick={() => this.props.restaurants.resetReservation()} className="reservation-clear">
-          Clear
-        </button>
+      <div>
+        <div className="reservation-complete">
+          <button
+            className={
+              this.props.restaurants.getReservedRestaurant.name ? 'reservation-confirm' : 'reservation-disabled'
+            }
+            onClick={
+              this.props.restaurants.getReservedRestaurant.name ? () => this.props.restaurants.toggleModal() : () => ''
+            }
+          >
+            Complete Reservation
+          </button>
+          <button onClick={() => this.props.restaurants.resetReservation()} className="reservation-clear">
+            Clear
+          </button>
+        </div>
+        <ConfirmationModal />
       </div>
     );
   }
--- src/components/Restaurants/Reservations/ConfirmationModal.jsx
+++ src/components/Restaurants/Reservations/ConfirmationModal.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
 /* import PropTypes from 'prop-types'; */
-import { observer, inject } from 'mobx-react';/* 
-import { restaurantsPropType, restaurantsPropTypeDefaults } from '../../../stores/Restaurants'; */
+import { observer, inject } from 'mobx-react';
+import { restaurantsPropType, restaurantsPropTypeDefaults } from '../../../stores/Restaurants';
 
 @inject('restaurants')
 @observer
 class ConfirmationModal extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleCloseModal = () => {
+    this.props.restaurants.toggleModal();
+    this.props.restaurants.resetReservation();
+  };
+
+  render() {
+    return this.props.restaurants.getModalState ? (
+      <div className="confirmation-modal">
+        <h2 className="confirmation-title">Reservation Confirmation</h2>
+        <p>Reservation number: {Math.floor(Math.random() * 1000) + 5000}</p>
+        <p>You have a reservation at {this.props.restaurants.getReservedRestaurant.name} on at pm for people.</p>
+        <button onClick={() => this.handleCloseModal()} className="reservation-confirm confirmation-close">
+          Close
+        </button>
+      </div>
+    ) : (
+      ''
+    );
+  }
 }
+
+ConfirmationModal.wrappedComponent.propTypes = {
+  restaurants: restaurantsPropType
+};
+
+ConfirmationModal.wrappedComponent.defaultProps = {
+  restaurants: restaurantsPropTypeDefaults
+};
 
 export default ConfirmationModal;
