340a8d90133a7b91c2c77d936f955cd64df37198
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-28 14:22:37 +0300

    added intro text to reservations

--- src/components/Restaurants/Reservations/ReservationsWrapper.jsx
+++ src/components/Restaurants/Reservations/ReservationsWrapper.jsx
@@ -8,6 +8,12 @@ import CompleteReservation from './CompleteReservation';
 const ReservationsWrapper = () => (
   <div className="reservations-container">
     <h1>Reserve your table</h1>
+    <p>
+      By booking a table, you&apos;re guaranteeing your place in one of our restaurants on a date and time of your
+      choice, so say goodbye to waiting times! Upon arrival, you can be rest assured that a member of our team will be
+      on hand to greet you, take your order and ensure you&apos;re looked after throughout your visit. To book, simply
+      select your chosen restaurant on a date and time of your choice and we&apos;ll do the rest.
+    </p>
     <SelectLocation />
     <SelectDate />
     <SelectTime />
