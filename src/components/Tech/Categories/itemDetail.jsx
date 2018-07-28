127eac913dd6be96dd19305cb7cf40f5592e330f
Martin Dobrudzhanski <martin.dobr@abv.bg>
2018-07-28 18:11:52 +0300

    component setings (#74)

--- src/components/Tech/Categories/itemDetail.jsx
+++ src/components/Tech/Categories/itemDetail.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ItemDetail = () => <div>ItemDetail</div>;
+const ItemDetail = ({ item }) =>
+  item.Title ? (
+    <div className="item-detail">
+      <p className="info" style={{ backgroundImage: `url(${item.photo})` }} />
+      <p className="info">{item.title}</p>
+      <p className="info plot">{item.text}</p>
+    </div>
+  ) : (
+    <div className="item-detail">
+      <div className="info">No selected item.</div>
+    </div>
+  );
+
+ItemDetail.propTypes = {
+  item: PropTypes.shape({
+    titel: PropTypes.string,
+    text: PropTypes.string,
+    photo: PropTypes.string
+  })
+};
+
+ItemDetail.defaultProps = {
+  item: {}
+};
 
 export default ItemDetail;
