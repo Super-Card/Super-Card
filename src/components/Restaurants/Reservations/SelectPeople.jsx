d6fee7d147476a8986c98b56acfbd4baae46e472
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-28 20:14:41 +0300

    added input field for number of people (#77)

--- src/components/Restaurants/Reservations/SelectPeople.jsx
+++ src/components/Restaurants/Reservations/SelectPeople.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { observer, inject } from 'mobx-react';
+import { restaurantsPropType, restaurantsPropTypeDefaults } from '../../../stores/Restaurants';
 
-const SelectPeople = () => (
-  <div>
-    <p>Select People</p>
-  </div>
-);
+@inject('restaurants')
+@observer
+class SelectPeople extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.restaurants.setSelectedPeople(event.target.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Select People</p>
+        <form>
+          <input type="number" min="1" max="10" step="1" onChange={this.handleChange} />
+        </form>
+      </div>
+    );
+  }
+}
+
+SelectPeople.wrappedComponent.propTypes = {
+  restaurants: restaurantsPropType
+};
+
+SelectPeople.wrappedComponent.defaultProps = {
+  restaurants: restaurantsPropTypeDefaults
+};
 
 export default SelectPeople;
