af194f231f9c64d635885071d4caec4db314d935
Martin Dobrudzhanski <martin.dobr@abv.bg>
2018-07-29 23:38:57 +0300

    some refactoring (#88)

--- src/pages/tech.jsx
+++ src/pages/tech.jsx
@@ -3,6 +3,10 @@ import CategoryContainer from '../components/Tech/Categories/CategoryContainer';
 
 const Tech = () => (
   <div>
+    <div className="heading-container">
+      <h2 className="category-header">Categories</h2>
+      <h2 className="discount-heading">See your discount today!</h2>
+    </div>
     <CategoryContainer />
   </div>
 );
