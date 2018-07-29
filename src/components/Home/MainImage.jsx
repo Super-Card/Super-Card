d46e791cbfb0d6995b67582f5e1e5d04bb84ccb2
martinski74 <martin.dobr@abv.bg>
2018-07-30 01:06:33 +0300

    home page

---
+++ src/components/Home/MainImage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MainImage = () => (
+  <div>
+    <img className="header-image" src="../../assets/home/heading-pic.jpeg" alt="img" />
+  </div>
+);
+
+export default MainImage;
