8c8e0ff136ced433b9619028766aba1ddf839f04
philipkovachev9 <philipkovachev9@yahoo.com>
2018-07-30 10:58:13 +0300

    I have implemented a countdown

---
+++ src/components/Auto/Clock/ClockApp.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Clock from './Clock';
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            deadline: 'August 05, 2018'
+            
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-title">Offer Expires: {this.state.deadline}</div>
+               <Clock
+               deadline = {this.state.deadline}
+               />
+                
+            </div>
+        )
+    }
+}
+
+export default App;
