c46da575799ecc1f9ab7079491d04b8b9ba81341
Philip Kovachev <36931694+philipkovachev9@users.noreply.github.com>
2018-07-30 11:11:11 +0300

    I have implemented a countdown (#92)

--- src/components/Auto/CardCheck/CardCheck.jsx
+++ src/components/Auto/CardCheck/CardCheck.jsx
@@ -5,7 +5,7 @@ const CardCheck = () => (
   <div className="newsletter-container">
     <h1>Enter the ID of your card:</h1>
     <div className="center">
-      <input type="email" />
+      <input type="number" />
       <input type="submit" value="Check" />
     </div>
   </div>
--- src/pages/auto.jsx
+++ src/pages/auto.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import CardCheck from '../components/Auto/CardCheck/CardCheck';
+import ClockApp from '../components/Auto/Clock/ClockApp';
 
 const Auto = () => (
   <div>
-    <CardCheck />
+    <CardCheck />,
+    <ClockApp/>
   </div>
 );
 
