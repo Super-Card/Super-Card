0725b318b46a71cdd7ebe963328aec2ecc7240e5
Martin Dobrudzhanski <mdobrtc@training.local>
2018-07-30 20:13:08 +0300

    form-fixed

--- src/components/Home/RequestForm.jsx
+++ src/components/Home/RequestForm.jsx
@@ -11,29 +11,48 @@ class RequestForm extends React.Component {
       }
     };
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleInputEmailChange = this.handleInputEmailChange.bind(this);
     this.saveUser = this.saveUser.bind(this);
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const name = target.name;
-    const value = target.value;
-    const user = this.state.user;
-
-    user[name] = value;
-
-    this.setState({ user });
+  handleInputChange({ name, value }) {
+    this.setState({ user: { name, value, ...this.state.user } });
+  }
+  handleInputEmailChange({ email, value }) {
+    this.setState({ user: { email, value, ...this.state.user } });
+  }
+  handleInputTextChange({ text, value }) {
+    this.setState({ user: { text, value, ...this.state.user } });
   }
   saveUser(event) {
     event.preventDefault();
-    alert('Your sumbition is sucessful!');
+     alert('Your sumbition is sucessful!');
   }
   render() {
     return (
       <form>
-        <input type="text" name="name" onChange={this.handleInputChange} value={this.state.user.name} />
-        <input type="email" name="email" onChange={this.handleInputChange} value={this.state.user.email} />
-        <textarea name="text" onChange={this.handleInputChange} cols="30" rows="5" value={this.state.user.text} />
+        <input
+          type="text"
+          name="name"
+          onChange={this.handleInputChange}
+          // value={this.state.user.name}
+          placeholder="Your name"
+        />
+        <input
+          type="email"
+          name="email"
+          onChange={this.handleInputEmailChange}
+          // value={this.state.user.email}
+          placeholder="Your email"
+        />
+        <textarea
+          name="text"
+          onChange={this.handleInputTextChange}
+          cols="30"
+          rows="5"
+          // value={this.state.user.text}
+          placeholder="Write message.."
+        />
         <input type="submit" value="Submit" onClick={this.saveUser} />
       </form>
     );
