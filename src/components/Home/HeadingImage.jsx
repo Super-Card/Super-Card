b0866e166f461865ec6aa41f0d58a508ba008a3c
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2018-07-30 20:53:26 +0300

    Feature/header footer styling (#104)
    * restyled navbar and footer
    
    * home page styling

--- src/components/Home/HeadingImage.jsx
+++ src/components/Home/HeadingImage.jsx
@@ -3,8 +3,11 @@ import './home.css';
 
 const HeadingImage = () => (
   <div>
-    <div className="head-image" style={{ backgroundImage: "url('../../../assets/home/heading-pic.jpg')" }}>
-      Get SuperCard and save money now!
+    <div className="head-image">
+      <p className="head-text">Get SuperCard and save money now!</p>
+      <div className="head-cta">
+        <button className="head-cta-btn">Get your card</button>
+      </div>
     </div>
   </div>
 );
