4a75c43e6db0958ffa54fe7718dbf5214a9323e2
martinski74 <martin.dobr@abv.bg>
2018-08-01 00:19:55 +0300

    added modal window

--- src/components/Home/RequestForm.jsx
+++ src/components/Home/RequestForm.jsx
@@ -8,13 +8,19 @@ class RequestForm extends React.Component {
         name: '',
         email: '',
         text: ''
-      }
+      },
+      modalOpened: false
     };
+
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleInputEmailChange = this.handleInputEmailChange.bind(this);
-    this.saveUser = this.saveUser.bind(this);
+    this.modalToggle = this.modalToggle.bind(this);
   }
 
+  modalToggle(e) {
+    e.preventDefault();
+    this.setState({ modalOpened: !this.state.modalOpened });
+  }
   handleInputChange({ name, value }) {
     this.setState({ user: { name, value, ...this.state.user } });
   }
@@ -24,44 +30,53 @@ class RequestForm extends React.Component {
   handleInputTextChange({ text, value }) {
     this.setState({ user: { text, value, ...this.state.user } });
   }
-  saveUser(event) {
-    event.preventDefault();
-    // this.refs.form.reset();
-  }
   render() {
+    const coverClass = this.state.modalOpened ? 'cover-modal cover-modal-active' : 'cover-modal';
+    const containerClass = this.state.modalOpened ? 'modal-wrapper modal-wrapper-active' : 'modal-container';
+
     return (
-      <form className="request-form" id="request-card">
-        <h2 className="request-heading">Request your SuperCard today!</h2>
-        <input
-          type="text"
-          name="name"
-          onChange={this.handleInputChange}
-          // value={this.state.user.name}
-          placeholder="Your name"
-          className="request-input"
-        />
-        <br />
-        <input
-          type="email"
-          name="email"
-          onChange={this.handleInputEmailChange}
-          // value={this.state.user.email}
-          placeholder="Your email"
-          className="request-input"
-        />
-        <br />
-        <textarea
-          name="text"
-          onChange={this.handleInputTextChange}
-          cols="45"
-          rows="5"
-          // value={this.state.user.text}
-          placeholder="Write message.."
-          className="request-input"
-        />
-        <br />
-        <input className="btn-submit" type="submit" value="Submit" onClick={this.saveUser} />
-      </form>
+      <div>
+        <form className="request-form" id="request-card">
+          <h2 className="request-heading">Request your SuperCard today!</h2>
+          <input
+            type="text"
+            name="name"
+            onChange={this.handleInputChange}
+            // value={this.state.user.name}
+            placeholder="Your name"
+            className="request-input"
+          />
+          <br />
+          <input
+            type="email"
+            name="email"
+            onChange={this.handleInputEmailChange}
+            // value={this.state.user.email}
+            placeholder="Your email"
+            className="request-input"
+          />
+          <br />
+          <textarea
+            name="text"
+            onChange={this.handleInputTextChange}
+            cols="45"
+            rows="5"
+            // value={this.state.user.text}
+            placeholder="Write message.."
+            className="request-input"
+          />
+          <br />
+          <button className="btn-submit" onClick={this.modalToggle}>
+            Submit
+          </button>
+        </form>
+        <div className={containerClass}>
+          <h1> Your request is accepted! </h1>
+          <p className="modal-description">We will get in tuch with you in a 24 hours to set-up delivery.</p>
+        </div>
+
+        <div className={coverClass} onClick={this.modalToggle} />
+      </div>
     );
   }
 }
