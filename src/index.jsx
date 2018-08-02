9098dd3847ad26a837d6e51bd90704abc09a67c3
Philip Kovachev <36931694+philipkovachev9@users.noreply.github.com>
2018-08-02 16:10:34 +0300

    I created a mobx-store, displayed some data and added some css (#129)

--- src/index.jsx
+++ src/index.jsx
@@ -6,19 +6,22 @@ import { Beauty } from './stores/Beauty';
 import { Tech } from './stores/Tech';
 import { Malls } from './stores/ClothesData';
 import App from './App';
+import { Auto } from './stores/Auto';
 
 const restaurants = new Restaurants();
 const beauty = new Beauty();
 const tech = new Tech();
 const mall = new Malls();
+const auto = new Auto();
 
 window.restaurants = restaurants;
 window.beauty = beauty;
 window.tech = tech;
 window.mall = mall;
+window.auto = auto;
 
 ReactDOM.render(
-  <Provider restaurants={restaurants} beauty={beauty} tech={tech} mall={mall}>
+  <Provider restaurants={restaurants} beauty={beauty} tech={tech} mall={mall} auto={auto}>
     <App />
   </Provider>,
   document.querySelector('.container')
---
+++ src/stores/Auto.jsx
@@ -0,0 +1,26 @@
+import { observable, action, computed } from 'mobx';
+
+class Auto {
+  @observable
+  auto = [
+    {
+        name:"Shop1"
+      },
+      {
+        name:
+        "Shop2"
+      },
+  ];
+
+  @action
+  addAuto(car) {
+    this.auto.push(car);
+  }
+
+  @computed
+  get carCount() {
+    return this.auto.length;
+  }
+}
+
+export { Auto };
