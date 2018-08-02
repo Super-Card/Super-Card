327490142ff8f27445896b6612d57d53ac3e33f1
philipkovachev9 <philipkovachev9@yahoo.com>
2018-08-02 16:06:53 +0300

    I created a mobx-store, displayed some data and added some css

--- src/components/Auto/CardCheck/CardCheck.jsx
+++ src/components/Auto/CardCheck/CardCheck.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import PropTypes from 'prop-types';
 import './cardCheck.css';
 
-const CardCheck = () => (
-  <div>
-    <div className="newsletter-container">
-      <h1>Enter the ID of your card:</h1>
-      <div className="center">
-        <input type="number" />
-        <input type="submit" value="Check" />
+@inject('auto')
+@observer
+class CardCheck extends Component {
+  render() {
+    const { auto } = this.props;
+
+    return (
+      <div>
+        <div className="newsletter-container">
+          <h1>Enter the ID of your card:</h1>
+          <div className="center">
+            <input type="number" />
+            <input type="submit" value="Check" />
+          </div>
+          <div className="results">{auto.carCount}</div>
+        </div>
+        <h1>Offers:</h1>
       </div>
-    </div>
-    <h1>Offers:</h1>
-  </div>
-);
+    );
+  }
+}
+
+CardCheck.propTypes = {
+  auto: PropTypes.shape({
+    carCount: PropTypes.string
+  })
+};
 
 export default CardCheck;
